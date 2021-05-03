7783691232d04917332bacfa22953863e4ca53a1
SavitaBeauty <savitabns25@gmail.com>
2021-05-04 00:55:04 +0530

    User Register Reducer, Action & Screen.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,14 @@ import {
     productListReducer, 
     productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReduce } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
-    userLogin: userLoginReduce,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse

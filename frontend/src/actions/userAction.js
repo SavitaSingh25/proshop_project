85cd2c63818b18e10060a1ccdd4a8d929fd5f128
SavitaBeauty <savitabns25@gmail.com>
2021-05-04 13:23:20 +0530

    Profile Screen & Get User Details.

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -6,7 +6,10 @@ import {
     USER_LOGOUT,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL
+    USER_REGISTER_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL
  } from '../constants/userConstant'
 
 export const login = (email, password) => async (dispatch) => {
@@ -42,6 +45,13 @@ export const login = (email, password) => async (dispatch) => {
                 : error.message,
         })
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -85,12 +95,40 @@ export const register = (name, email, password) => async (dispatch) => {
 }
 
 
-export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo')
-    dispatch({
-        type: USER_LOGOUT
-    })
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState() 
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        }
+
+        const { data } = await axios.get(`/api/users/${id}`, config)
+
+            dispatch({
+                type: USER_DETAILS_SUCCESS,
+                payload: data,
+            })
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload:
+                error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        })
+    }
 }
+
+
+
  
 
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import {
     productListReducer, 
     productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -13,6 +13,7 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse

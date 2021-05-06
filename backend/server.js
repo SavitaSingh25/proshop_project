3588403b88e0f620134602aad9c57995f7ccf531
SavitaBeauty <savitabns25@gmail.com>
2021-05-06 17:02:37 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -15,12 +16,24 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+
+
+const __dirname = path.resolve()
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))  
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(notFound)
 

45a96247f557bbfbc366de4975e8bcb35d57ce08
SavitaBeauty <savitabns25@gmail.com>
2021-06-11 01:31:01 +0530

    Create Order.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,9 @@ import {
     userDetailsReducer, 
     userUpdateProfileReducer 
 } from './reducers/userReducers'
+import {
+    orderCreateReducer
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -20,6 +23,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    orderCreate: orderCreateReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse

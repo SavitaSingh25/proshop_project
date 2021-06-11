95d67cde4d4552bd1af602bb6949bdc22e868e88
SavitaBeauty <savitabns25@gmail.com>
2021-06-11 14:13:53 +0530

    Order Details Reducer & Action.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,8 @@ import {
     userUpdateProfileReducer 
 } from './reducers/userReducers'
 import {
-    orderCreateReducer
+    orderCreateReducer,
+    orderDetailsReducer 
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -24,6 +25,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse

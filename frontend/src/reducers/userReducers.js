d3ccec5c10139af386f3d56bee104a872cdbd6f9
savita <savitabns25@gmail.com>
2021-07-01 04:11:22 +0530

    Admin Screen Access Security.

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -15,7 +15,8 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_LIST_REQUEST,
     USER_LIST_SUCCESS,
-    USER_LIST_FAIL
+    USER_LIST_FAIL,
+    USER_LIST_RESET
  } from '../constants/userConstant'
 
 export const  userLoginReducer = (state = {}, action) => {
@@ -83,6 +84,8 @@ export const userListReducer = (state = { users: [] }, action) => {
             return { loading: false, users: action.payload }
         case USER_LIST_FAIL:
             return { loading: false, error: action.payload }
+        case USER_LIST_RESET: 
+            return { users: [] }
         default:
             return state
     }
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,16 +5,25 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listUsers } from '../actions/userAction'
+import { USER_LIST_RESET } from '../constants/userConstant'
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
 
     const userList = useSelector((state) => state.userList)
     const { loading, error, users } = userList
 
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+
     useEffect(() => {
-        dispatch(listUsers())
-    }, [dispatch])
+        if(userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else { 
+            history.push('/login')
+        }
+       
+    }, [dispatch, history])
 
     const deleteHandler = (id) =>{
         console.log('delete')
